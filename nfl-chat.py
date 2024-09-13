37274e60275f8675af70d24e8797b9f06f1e6eb6
rdraude <ryand0827@outlook.com>
2024-09-13 15:03:47 -0400

    Reformatting

--- nfl-chat.py
+++ nfl-chat.py
@@ -1,15 +1,21 @@
 import os
 from openai import OpenAI
 
+# Retrieve the API token from environment variables
 token = os.environ["GITHUB_TOKEN"]
+
+# Define the endpoint and model name
 endpoint = "https://models.inference.ai.azure.com"
 model_name = "gpt-4o"
 
+# Initialization
 client = OpenAI(
     base_url=endpoint,
     api_key=token,
 )
 userQ = input("Enter your question: ")
+
+# Query the API
 response = client.chat.completions.create(
     messages=[
         {
@@ -23,5 +29,4 @@ response = client.chat.completions.create(
     ],
     model=model_name,
 )
-
 print(response.choices[0].message.content)
